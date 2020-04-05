c4c130bf39a387c58b82ed63358c5fe39aecd16c
liuguang <1072941868@qq.com>
2020-04-05 21:59:10 +0800

    listView使用

---
+++ lib/model/post.dart
@@ -0,0 +1,54 @@
+class Post{
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title:'天龙八部',
+    author: '乔帮主',
+    imageUrl: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2218971900,3495260971&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title:'moka li',
+    author: '小明',
+    imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586103109458&di=c507c1d89bf7ecc9dea686c6f5b7634e&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2018-01-27%2F5a6be4eb5a567.jpg',
+  ),
+  Post(
+    title:'merlin di',
+    author: '小明',
+    imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586103192335&di=52ea6de491e396930cc9856e01ca5d51&imgtype=0&src=http%3A%2F%2Fimg1.imgtn.bdimg.com%2Fit%2Fu%3D2136119266%2C167105823%26fm%3D214%26gp%3D0.jpg',
+  ),
+  Post(
+    title:'mokae li',
+    author: '小d明',
+    imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586103109454&di=9cc00da7907f4cff5ee456affa72b8c0&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F3%2F58d3341f0cfe9.jpg',
+  ),
+  Post(
+    title:'arthur',
+    author: '小hua',
+    imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586103109450&di=5e2f939d3bfbf7cba7f0b3cf5a8b47b3&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201604%2F09%2F20160409145439_WhVyd.jpeg',
+  ),
+  Post(
+    title:'braer',
+    author: 'dulaj',
+    imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586103109503&di=ef29f4bd98f371f3718686945ddd40e5&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2018-05-18%2F5afe6831f2056.jpg',
+  ),
+  Post(
+    title:'minging',
+    author: 'tianken',
+    imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586103109500&di=20a4c13365be88c5a6a6745d50a85aed&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F1%2F59af671727974.jpg',
+  ),
+  Post(
+    title:'jingteng',
+    author: '小hula',
+    imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586103450519&di=e28277f0ca342308216e91ab31685b53&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201608%2F27%2F20160827172726_GJfX2.jpeg',
+  ),
+];
